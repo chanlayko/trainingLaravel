ef68868396c9adf0be86efee27839e554fb0288f
chanlayko <chanlayko125@gmail.com>
2021-07-17 14:31:27 +0630

    week6.1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Test;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
@@ -23,6 +24,14 @@ Route::get('/',[HomeController::class,'index']);
 Route::get('about',[HomeController::class,'about']);
 Route::get('contact',[HomeController::class,'contact']);
 
+Route::get('/',function(Test $test){
+    // app()->bind('test',function(){
+    //     return new Test();
+    // });
+
+    // $data = resolve($test);
+    dd($test);
+});
 Route::resource('/post',PostController::class)->middleware('auth');
 
 Route::get('logout',[AuthController::class,'logout']);
